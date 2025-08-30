ccba75d82c338a9270d4e8384d945c255a899e9a
zchengfa <1522977814@qq.com>
2025-08-30 09:58:41 +0800

    docs(add dist):

--- docs/.vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from "vitepress/theme";
-import ImageMagnifier from "../components/ImageMagnifier.vue";
-
-export default {
-    ...DefaultTheme,
-    enhanceApp: ({app})=>{
-        app.component("ImageMagnifier", ImageMagnifier);
-    }
-}
